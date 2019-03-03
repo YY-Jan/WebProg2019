9a4b92ddd79f95d22dc8e661be910a5883fbb4c6
doraeric <benson.doraemon@gmail.com>
2019-03-03 20:43:16 +0800

    add support for youtube

--- Practice02/own/main.js
+++ Practice02/own/main.js
@@ -7,6 +7,7 @@ var img_list = [
         source: 'https://www.flickr.com/photos/othree/24960910089/in/photolist-EpKxfQ-ExT9B3-E2HbWM-F4Jamq-FQZXNb-FTj9cP-FTj8Pz-Fz4onQ-FZaQUr-F4JbVY-F4UYUR-od397q-F4V1PT-FWT12G-FR13Y1-Fz4kJo-T7GhCr-FWSYgN-Fz4qLY-F4Jhsj-of54qK-Fz4uxA-F4JiNf-Fz4uJs-FTjbMi-eyTfoc-FR17nf-FTjdxH-Fz4ofq-F4V6eK-FWT9Qd-FR16mY-FZaZyF-FTjm3H-FR13Pd-FTjmck-FWT7ew-FTjnAx-eyWfX5-eySc8F-FQZXX9-F4JpZU-FWT5hA-FR1crU-Fz4Nww-F4JF2s-FZb51M-S5gfRn-F4Vpsi-F4VezH/'},
     {src: 'https://farm2.staticflickr.com/1832/29118231217_ce9b7fa4af_k.jpg', type: 1,
         source: 'https://www.flickr.com/photos/hitcon/29118231217/'},
+    {src: '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/9ss0iRp9RGM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>', type: 2, source: 'https://www.youtube.com/watch?v=9ss0iRp9RGM'},
 ]
 
 function img_init() {
@@ -53,6 +54,9 @@ function change_img() {
                 img.innerHTML = '<img src="'+img_list[img_id].src+'" alt="">';
             }
         }
+    }
+    else if (img_list[img_id].type === 2) {
+        img.innerHTML = img_list[img_id].src;
     }
     img_source.href = !img_list[img_id].source ? img_list[img_id].src : img_list[img_id].source;
     if (img_id <= 0)
