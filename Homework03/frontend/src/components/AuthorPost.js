19f17beaa200809a7631bf85ddc8131fee8a1b86
doraeric <benson.doraemon@gmail.com>
2019-06-09 14:38:09 +0800

    [homework03] collapse cards

--- Homework03/frontend/src/components/AuthorPost.js
+++ Homework03/frontend/src/components/AuthorPost.js
@@ -1,27 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-import { Card, CardHeader, CardBody } from 'reactstrap'
+import { Card, CardHeader, CardBody, Collapse } from 'reactstrap'
 import Post from './Post'
 
-const AuthorPost = ({
-  className,
-  data: {
-    id,
-    name,
-    posts,
+class AuthorPost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { open: false };
   }
-}) => {
-  const posts_dom = posts.map((elem) => {
-    return <Post key={elem.id} data={elem} />;
-  });
-  return (
-    <Card className={className}>
-      <CardHeader>{name}</CardHeader>
-      <CardBody>
-        {posts_dom}
-      </CardBody>
-    </Card>
-  )
+  render() {
+    const { className,
+      data: {
+        id,
+        name,
+        posts,
+      }} = this.props;
+    const posts_dom = posts.map((elem) => {
+      return <Post key={elem.id} data={elem} />;
+    });
+    return (
+      <Card className={className}>
+        <CardHeader style={{cursor: 'pointer'}}
+          onClick={e => this.setState(prev => ({open: !prev.open}))}>{name} ({posts.length})</CardHeader>
+        <Collapse isOpen={this.state.open}>
+          <CardBody>
+            {posts_dom}
+          </CardBody>
+        </Collapse>
+      </Card>
+    )
+  }
 }
 
 export default AuthorPost
